55bf35dfd869c84dfeeb23ae5f840f50ff2dc9d9
Ivan Fernandez <ivanfernandez@macbook-pro-de-ivan.local>
2016-08-17 12:58:39 +0200

    versión 1

---
+++ JuegoDeMemoria/JuegoDeMemoria.swift
@@ -0,0 +1,35 @@
+//
+//  JuegoDeMemoria.swift
+//  JuegoDeMemoria
+//
+//  Created by Ivan Fernandez on 17/8/16.
+//  Copyright © 2016 Ivan Fernandez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+for i in 0  ..< 101  {
+    switch i{
+    case 1...29:
+        if i%5 == 0 {
+            print("\(i) Bingo!!!")
+        }
+        if i%2 == 0 {
+            print("\(i) Par")
+        } else {
+            print("\(i) Impar")
+        }
+    case 41...100:
+        if i%5 == 0 {
+            print("\(i) Bingo!!!")
+        }
+        if i%2 == 0 {
+            print("\(i) Par")
+        } else {
+            print("\(i) Impar")
+        }
+    default:
+        print("Viva swift")
+    }
+}
